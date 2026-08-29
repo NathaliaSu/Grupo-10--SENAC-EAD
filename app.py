6652fc14ddea2dd6c12cc594cefaf5de696585aa
williamgo05 <gomes.william18@yahoo.com.br>
2026-08-29 16:39:01 -0300

    fix default main

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#Default
+
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
@@ -10,16 +12,4 @@ dados = pd.read_csv(
 
 dados = dados.drop(["Student_ID","Age","Department"], axis=1)
 
-# print(dados.head())
-media = dados.groupby("Depression")["Social_Media_Hours"].mean()
-st.subheader("Social Media x Depression")
-fig, ax = plt.subplots()
-media.plot.bar(ax=ax)
-st.pyplot(fig)
-
-sem_depressão = dados[dados["Depression"] == False]
-com_depressão = dados[dados["Depression"] == True]
-
-print(sem_depressão["Social_Media_Hours"].median())
-print(com_depressão["Social_Media_Hours"].median())
-
+#Default
