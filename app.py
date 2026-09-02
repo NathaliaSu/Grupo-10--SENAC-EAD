1ab96854ad68c9bae8f79da5e191bba0cc9e2ffd
fauzzztino <gabriel.kaneki2003@gmail.com>
2026-09-02 19:46:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 st.title("Rede Social x Estresse")
+
 df = pd.read_csv("base_tratada.csv")
 df_grafico = df.drop_duplicates(subset=["Social_Media_Hours"]).sample(10, random_state=42)
 df_grafico = df_grafico.sort_values(by="Social_Media_Hours")
@@ -13,11 +14,18 @@ def formata_hora(valor):
     return f"{h}:{m:02d}"
 
 rotulos_tempo = [formata_hora(t) for t in df_grafico["Social_Media_Hours"]]
+
 fig, ax = plt.subplots(figsize=(10, 4))
+
+fig.patch.set_alpha(0)
+ax.patch.set_alpha(0)
+
 ax.bar(range(10), df_grafico["nivel_estresse"], color="#1f77b4")
+
 ax.set_title("Rede Social x Estresse")
 ax.set_xlabel("Tempo de uso de Redes Sociais")
 ax.set_ylabel("Nível de Estresse")
 ax.set_xticks(range(10))
 ax.set_xticklabels(rotulos_tempo)
+
 st.pyplot(fig)
